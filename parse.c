0acbc427a613957eb5dd3a1ac6edc2fc9b5a0151
k-Arihara <dev9876dora@gmail.com>
2022-06-23 03:01:33 +0900

    [fix] 変数の文字の数字が対応していないバグを修正

--- parse.c
+++ parse.c
@@ -81,7 +81,7 @@ bool startswith(char* p, char* q) {
 }
 
 int is_alnum(char c){
-  return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || (c == '_');
+  return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') ||  ('0' <= c && c <= '9') || (c == '_');
 }
 
 Token* tokenize(char* p) {
